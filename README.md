d731aec86c1f6cd41faa7f7eefd27fcfdd00d422
Jane Sandberg <js7389@princeton.edu>
2024-04-09 15:58:30 -0700

    Create an application with lux components

---
+++ README.md
@@ -0,0 +1,19 @@
+# Vue 3 application with some lux components
+
+This example application demonstrates importing
+lux components into individual components in a Vue 3
+application.  This allows vite to tree-shake
+lux, packaging only the components it needs in the
+bundle.
+
+## Trying out this application in the browser
+
+```
+npm install
+npm run dev
+```
+
+You can then see the application in your browser
+at the port that vite provides.
+
+## Confirming that vite can tree-shake lux
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+import { LuxInputButton, LuxIconApproved, LuxIconBase } from 'lux-design-system'
+
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <lux-input-button variation="solid" size="small">Apply Changes</lux-input-button>
+      <lux-dropdown-menu type="links" button-label="Select Options" :menu-items="[
+      {name: 'Vegetable'},
+      {name: 'Fruit', children: [
+        {name: 'Apple'},
+        {name: 'Pear'},
+      ]},
+    ]">
+    </lux-dropdown-menu>
+    My icon:
+    <lux-icon-base width="12" height="12" icon-name="Approved">
+      <lux-icon-approved></lux-icon-approved>
+    </lux-icon-base>
+  <h1>{{ msg }}</h1>
+</template>
